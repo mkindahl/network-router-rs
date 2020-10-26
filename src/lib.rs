3043d89ee4a8b7249f7996786ae5ee2c4ce0ac0c
Mats Kindahl <mats.kindahl@gmail.com>
2020-10-26 19:48:14 +0100

    Add clippy workflow check and remove feature inner attribute
    This commit adds a clippy workflow check and removes the use of
    `#![feature(async_closure]` since the code compiles and tests passes
    without it.

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,6 @@
 // implied.  See the License for the specific language governing
 // permissions and limitations under the License.
 
-#![feature(async_closure)]
-
 #[macro_use]
 extern crate log;
 
