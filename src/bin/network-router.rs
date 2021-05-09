7bebf107f6702633c025278e41b8b736ea6593b4
Mats Kindahl <mats.kindahl@gmail.com>
2021-05-09 14:09:59 +0200

    Update copyright notice

--- src/bin/network-router.rs
+++ src/bin/network-router.rs
@@ -1,4 +1,4 @@
-// Copyright 2019 Mats Kindahl
+// Copyright 2019-21 Mats Kindahl
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you
 // may not use this file except in compliance with the License.  You
